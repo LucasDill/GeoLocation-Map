a83a1abf9d5896d8e7b60906f37bf784f8180f6c
LucasDill <ldillist@lakeheadu.ca>
2020-05-01 16:16:08 -0600

    Commented and cleaned the last known well page

--- src/pages/last-known-well/last-known-well.ts
+++ src/pages/last-known-well/last-known-well.ts
@@ -11,24 +11,18 @@ import { PatientLocationPage } from '../patient-location/patient-location';
 })
 export class LastKnownWellPage {
 
-Height:any=40;
-myDate=moment().format("HH:mm");
-timeForm =new FormGroup({
-  time1: new FormControl('',Validators.required),
+myDate=moment().format("HH:mm");//sets the current time option for the last known well based on the time of the machine 
+timeForm =new FormGroup({//creates a new form with the last known well 
+  time1: new FormControl('',Validators.required),//set the form time with valdators required so they need to be entered in order to continue 
 });
   constructor(public navCtrl: NavController, public formBuilder: FormBuilder,public Data: DataServiceProvider) {
    //console.log(this.myDate);//Use of the current machine time for the initial timer value 
   }
   
-  goToLocation(params){
-  if (!params) params = {};
-    this.navCtrl.push(PatientLocationPage);
-}
-SubmitTime(params){
-  console.warn(this.timeForm.value);
-  if (!params) params = {};
-    this.navCtrl.push(PatientLocationPage);
-    this.Data.time=this.timeForm.value.time1;
+SubmitTime(params){//once the button is clicked to go to the next page it will push to the PatientLocationPage
+  if (!params) params = {};//set the parameters to null if there are none 
+    this.navCtrl.push(PatientLocationPage);//go to the next page 
+    this.Data.time=this.timeForm.value.time1;//set the time on the data page which will start the tier 
     if(this.Data.LastKnownWellTime!=this.timeForm.value.time1)//only stop if a new a new time is provided 
     {
       clearInterval(this.Data.intervalID);//stops the previous interval from running 
