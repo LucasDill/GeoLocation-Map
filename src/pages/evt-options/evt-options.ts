21d49aea8c61b3b4618dc8ad3f48d6f252d80023
LucasDill <ldillist@lakeheadu.ca>
2020-05-03 16:42:30 -0600

    - Commented and Cleaned the evt option page - added the missing weather display - changed some div id's as they where the same and not being used

--- src/pages/evt-options/evt-options.ts
+++ src/pages/evt-options/evt-options.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {  NavController, NavParams } from 'ionic-angular';
 import { MapPage } from '../map/map';
 import { DataServiceProvider } from '../../providers/data-service';
 import { RoutingProvider } from '../../providers/routing';
@@ -19,7 +19,7 @@ import { RoutingProvider } from '../../providers/routing';
 })
 export class EvtOptionsPage {
   cards: any;
-  evtSpinner: Boolean=true;
+  evtSpinner: Boolean=true;//sets the spinner option to true and the others to false so at the start the spinner shows and not the rest 
   evtshow: Boolean=false;
   evtEmpty:Boolean=false;
   message:any;
@@ -28,62 +28,54 @@ export class EvtOptionsPage {
     public Routes: RoutingProvider) {
    //   console.log(this.Routes.ImgRoutes);
   }
-  async ionViewWillLoad(){
-    var evt=await this.EVTsetup();//for the EVT capable hospitals
-    console.log(evt);
-    if(evt.length==0)
+  async ionViewWillLoad(){//this is one of the ionic lifecycle elements that happen when the page is about to load 
+    var evt=await this.EVTsetup();//for the EVT capable hospitals 
+    //console.log(evt);
+    if(evt.length==0)//if there are no EVT routes returned 
     {
-      this.evtEmpty=true;
+      this.evtEmpty=true;// set the empty to be true and pass in a message that will be displayed where the cards usually would be 
       this.message="You appear to already be at an EVT Capable Hospital \nPlease Consult Hospital Regulations for further instructions";
     }
-    else{
+    else{//If there are routes returned set the cards to reflect those routes at the moment the TRHSC is the only site that is capable of performing EVT 
       this.cards=evt;
     }
     
    }
 
    async EVTsetup(){//EVT at the moment is just Thunder Bay which is the only bRegionalStrokeCenter
-    var evtRoutes;
-    await this.Routes.getRoutes("bRegionalStrokeCentre").then(data =>{
-      evtRoutes=data;
+    var evtRoutes;//sets a variable for all of the evt routes 
+    await this.Routes.getRoutes("bRegionalStrokeCentre").then(data =>{//use the getRoutes function from the routing provider to search for all of the RegionalStrokeCenters which at the moment is just TBRHSC
+      evtRoutes=data;//assign the evtRoutes to the data returned function 
     });
-    await this.Routes.nearestLocations(); 
-    var flightcards;
+    await this.Routes.nearestLocations(); //Wait for the Routing provider to get the closest Helipad and Airport to the starting and ending location 
+    var flightcards;//create a new varable to hold the information for the cards that deal with the helicopters and airplanes 
     await this.Routes.getFlights(evtRoutes).then(distances =>{
-      flightcards=distances;
+      flightcards=distances;//Have the routing Provider get information for the cards including the distances and the times needed 
        });
        evtRoutes=this.Routes.addRoutes(evtRoutes,flightcards);//add the elements of the flights to the end 
-       evtRoutes=this.Routes.masterSort(evtRoutes);
-       evtRoutes=this.Routes.SetColour(evtRoutes);
-       this.evtSpinner=false;
-       this.evtshow=true;
-       return evtRoutes;
+       evtRoutes=this.Routes.masterSort(evtRoutes);//The masterSort function will sort the options based on how long they take and take out any that have the same name to avoid getting routes to the origin location 
+       evtRoutes=this.Routes.SetColour(evtRoutes);//Set the colour of each of the cards based on when the patient is sheduled to arrive 
+       this.evtSpinner=false;//stop the spinner 
+       this.evtshow=true;//show the div that contains all of the cards 
+       return evtRoutes;//return the final list of cards to be displayed on the page 
   }
   
   @ViewChild('treatment-heading6') myInput: ElementRef;
   @ViewChild('weather') myInput2: ElementRef;
 
-  resize() {
-      this.myInput.nativeElement.style.height = this.myInput.nativeElement.scrollHeight + 'px';
-      this.myInput2.nativeElement.style.height = this.myInput2.nativeElement.scrollHeight + 'px';
+
+  goToRoute(params){//If the user clicks on a route that includes driving 
+    this.Data.ComplexRoute=false;//set the complex route to false 
+    this.Data.Destination=params;//pass the destination selected to the Data Provider 
+    this.navCtrl.push(MapPage);//Go to the map page to display the selected route
   }
 
-  goToRoute(params){
-    if (!params) params = {};
-    console.log(params);
-    this.Data.ComplexRoute=false;
-    this.Data.Destination=params;
-    this.navCtrl.push(MapPage);
-//this.Routes.nearestLocations("Landing Sites");
 
-  }
-  ComplexRoute(cardDat)
+  ComplexRoute(cardDat)// if the user selects a route that includes driving then flying it is considered a ComplexRoute
   {
-    if(!cardDat) cardDat={};
-    console.log(cardDat);
-    this.Data.ComplexRoute=true;
-    this.Data.Destination=cardDat;
-    this.navCtrl.push(MapPage);
+    this.Data.ComplexRoute=true;//set complexroute to be true 
+    this.Data.Destination=cardDat;//set the destination as the card that was selected 
+    this.navCtrl.push(MapPage);//push the user to the map page to see the route 
   } 
 }
 
