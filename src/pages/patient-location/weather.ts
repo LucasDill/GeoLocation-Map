a4707929ce09984f94339feb50494c4efc77a44b
LucasDill <ldillist@lakeheadu.ca>
2020-05-04 13:50:09 -0600

    Deleted unused page and commented on other one

--- src/pages/patient-location/weather.ts
+++ src/pages/patient-location/weather.ts
@@ -8,7 +8,7 @@ export class WeatherService {
 
   getWeatherFromApi(lat: string, lon: string){
     //return this.httpClient.get(`http://api.weatherstack.com/current?access_key=3b527b7fc289c02197c55fdc41d37d0e&query=${city}`);
-    return this.httpClient.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=cac781becd280a861602a53956e3b3b0`);
+    return this.httpClient.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=cac781becd280a861602a53956e3b3b0`);//get the weather information based on the lat and long 
     
   }
 
