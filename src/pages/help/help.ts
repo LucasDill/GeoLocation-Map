2eeedd93bcb16c19478c047dc0bbae5a63f8e34b
LucasDill <ldillist@lakeheadu.ca>
2020-05-04 11:09:06 -0600

    Added some explination for the help page

--- src/pages/help/help.ts
+++ src/pages/help/help.ts
@@ -6,7 +6,7 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'help.html'
 })
 export class HelpPage {
-
+//This page is also for the menu that we ended up removing because it made the navigation too confusing
   constructor(public navCtrl: NavController) {
   }
   
