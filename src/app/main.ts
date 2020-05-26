c1b166ecd43e7ec73858807a1389ed348f0640a0
LucasDill <ldillist@lakeheadu.ca>
2020-05-26 15:13:12 -0600

    32 binaries uploaded

--- src/app/main.ts
+++
@@ -1,5 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';// this is standard across many ionic applications 
-
-import { AppModule } from './app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
--- src/components/components.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { ComponentsTimerComponent } from './components-timer/components-timer';
-import { SpinnerComponent } from './spinner/spinner';
-@NgModule({
-	declarations: [],
-	imports: [],
-	exports: []
-})
-export class ComponentsModule {}
