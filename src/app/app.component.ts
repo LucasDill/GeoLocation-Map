09919e1ca60ba571e9d43932a6c1a6043447f9eb
LucasDill <ldillist@lakeheadu.ca>
2020-06-24 18:29:34 -0600

    - finalized and tested the start over functionality - Created the best practice functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { LastKnownWellPage } from '../pages/last-known-well/last-known-well';
 import { MapPage } from '../pages/map/map';
 import { DataServiceProvider } from '../providers/data-service';
 import { MapExplorePage } from '../pages/map-explore/map-explore';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 
 
@@ -18,7 +19,7 @@ export class MyApp {
     splash = true;
     rootPage:any;
 
-  constructor(private platform: Platform, private statusBar: StatusBar, private splashScreen: SplashScreen, private config: Config, private Data: DataServiceProvider) {
+  constructor(private platform: Platform, private statusBar: StatusBar, private splashScreen: SplashScreen, private config: Config, private Data: DataServiceProvider,private inAppBrowser: InAppBrowser) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -37,10 +38,21 @@ export class MyApp {
   }
 
   goToMap(){
-    console.log("The menu is working");
-    this.navCtrl.push(MapExplorePage);
+    this.navCtrl.push(MapExplorePage);// starts the map page for exploration 
   }
 
+  StartOver(){
+document.location.href='index.html';// starts the application over from the start 
+  }
+
+  goToContact(){
+    console.log("contact");
+  }
+
+  goToBestPractice(){
+    var url="https://www.strokebestpractices.ca/recommendations/acute-stroke-management";
+    const browser=this.inAppBrowser.create(url,'_self');
+  }
   ionViewDidLoad(){// once the view loads set the root page after three seconds so the animation can play and variables can be set up 
     setTimeout(()=> {
       this.splash = false;
